44fc27759f015dd53929b6de48b0ed72a9104c5a
Michel Soares <168469501+UNI-MichelSoares@users.noreply.github.com>
2024-11-11 20:05:28 -0300

    Add files via upload

---
+++ controle/ControlaPaciente.java
@@ -0,0 +1,132 @@
+package controle;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import dominio.Paciente;
+
+public class ControlaPaciente {
+	
+	private ArrayList<Paciente> pacientes;
+	
+	public ControlaPaciente() {
+		this.pacientes = new ArrayList<>();
+	}
+	
+	public void cadastrarPaciente(int id, String nome, double peso, double altura) {
+		Paciente paciente = new Paciente(id, nome, peso, altura);
+		pacientes.add(paciente);
+		System.out.println("Paciente cadastrado com sucesso!");
+	}
+	
+	public void editarPaciente(int id) {
+		Paciente paciente = buscarPacientePorId(id);
+		if (paciente != null) {
+			Scanner scanner = new Scanner(System.in);
+			
+			System.out.print("Novo nome:");
+			String novoNome = scanner.nextLine();
+			System.out.print("Novo peso:");
+			double novoPeso = scanner.nextDouble();
+			System.out.print("Nova altura:");
+			double novaAltura = scanner.nextDouble();
+			
+			paciente.setNome_paciente(novoNome);
+			paciente.setPeso_paciente(novoPeso);
+			paciente.setAltura_paciente(novaAltura);
+			
+			System.out.println("Paciente editado com sucesso!");
+			
+			scanner.close();
+		}
+		else {
+			System.out.println("Paciente de ID:" +id +" não encontrado!");
+		}
+	}
+	
+	public void excluirPaciente(int id) {
+		Paciente paciente = buscarPacientePorId(id);
+		if (paciente != null) {
+			pacientes.remove(paciente);
+			System.out.println("Paciente excluido com sucesso!");
+		} 
+		else {
+			System.out.println("Paciente de ID: " +id +" não encontrado.");
+		}
+	}
+	
+	public void exibirPacientes() {
+		if (pacientes.isEmpty()) {
+			System.out.println("Nenhum paciente encontrado.");
+		}
+		else {
+			System.out.println("Lista de pacientes: ");
+			for (Paciente paciente : pacientes) {
+				paciente.listarPacientes();
+			}
+		}
+	}
+	
+	public Paciente buscarPacientePorId(int id) {
+		for (Paciente paciente : pacientes) {
+			if (paciente.getId_paciente() == id) {
+				return paciente;
+			}
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		
+		ControlaPaciente controle = new ControlaPaciente();
+		Scanner scanner = new Scanner(System.in);
+		int opcao;
+		
+		do {
+			System.out.println("\nMenu:");
+            System.out.println("1. Adicionar paciente");
+            System.out.println("2. Exibir paciente");
+            System.out.println("3. Editar paciente");
+            System.out.println("4. Excluir paciente");
+            System.out.println("0. Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+            
+            switch (opcao) {
+            case 1:
+            	System.out.print("ID do paciente: ");
+            	int id = scanner.nextInt();
+            	System.out.print("Nome do paciente: ");
+            	String nome = scanner.nextLine();
+            	System.out.print("Peso do paciente: ");
+            	double peso = scanner.nextDouble();
+            	System.out.print("Altura do paciente: ");
+            	double altura = scanner.nextDouble();
+            	
+            	controle.cadastrarPaciente(id, nome, peso, altura);
+            	break;
+            case 2:
+            	controle.exibirPacientes();
+            	break;
+            case 3:
+            	System.out.print("ID do paciente: ");
+            	int idAlterar = scanner.nextInt();
+            	controle.editarPaciente(idAlterar);
+            	break;
+            case 4:
+            	System.out.print("ID do paciente: ");
+            	int idExcluir = scanner.nextInt();
+            	controle.excluirPaciente(idExcluir);
+            	break;
+            case 0:
+            	System.out.println("Encerrando o programa.");
+            default:
+            	System.out.println("Opção inválida.");
+            }
+            
+		} while (opcao != 0);
+		
+		scanner.close();
+	}
+	
+}
---
+++ dominio/Paciente.java
@@ -0,0 +1,51 @@
+package dominio;
+
+public class Paciente {
+	
+	private int id_paciente;
+	private String nome_paciente;
+	private double peso_paciente;
+	private double altura_paciente;
+	
+	// c
+	public Paciente(int id, String nome, double peso, double altura) {
+		this.id_paciente = id;
+		this.nome_paciente = nome;
+		this.peso_paciente = peso;
+		this.altura_paciente  = altura;
+	}
+	
+	// Métodos get e set:
+	public int getId_paciente() {
+		return id_paciente;
+	}
+	public void setId_paciente(int id_paciente) {
+		this.id_paciente = id_paciente;
+	}
+	
+	public String getNome_paciente() {
+		return nome_paciente;
+	}
+	public void setNome_paciente(String nome_paciente) {
+		this.nome_paciente = nome_paciente;
+	}
+	
+	public double getPeso_paciente() {
+		return peso_paciente;
+	}
+	public void setPeso_paciente(double peso_paciente) {
+		this.peso_paciente = peso_paciente;
+	}
+	
+	public double getAltura_paciente() {
+		return altura_paciente;
+	}
+	public void setAltura_paciente(double altura_paciente) {
+		this.altura_paciente = altura_paciente;
+	}
+	
+	public void listarPacientes() {
+		System.out.println("ID: " +id_paciente +"Nome: " +nome_paciente +"Peso: " +peso_paciente +"Altura: " +altura_paciente);
+	}
+	
+}
